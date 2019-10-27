06b3e9fe0751d82d9611ad4d43c077c924803ccc
SeppPenner <HansM2013@gmx.de>
2019-10-27 20:00:14 +0100

    Updated nuget packages, added GitVersionTask.

--- LogCleanClient/Properties/AssemblyInfo.cs
+++ LogCleanClient/Properties/AssemblyInfo.cs
@@ -23,17 +23,3 @@ using System.Runtime.InteropServices;
 // Die folgende GUID bestimmt die ID der Typbibliothek, wenn dieses Projekt für COM verfügbar gemacht wird
 
 [assembly: Guid("a5a23f19-0ee1-4e3a-9e17-b24b4692c958")]
-
-// Versionsinformationen für eine Assembly bestehen aus den folgenden vier Werten:
-//
-//      Hauptversion
-//      Nebenversion 
-//      Buildnummer
-//      Revision
-//
-// Sie können alle Werte angeben oder die standardmäßigen Build- und Revisionsnummern 
-// übernehmen, indem Sie "*" eingeben:
-// [assembly: AssemblyVersion("1.0.*")]
-
-[assembly: AssemblyVersion("1.0.0.6")]
-[assembly: AssemblyFileVersion("1.0.0.6")]
