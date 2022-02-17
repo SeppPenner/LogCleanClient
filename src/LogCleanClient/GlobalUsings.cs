61a1489254d624e786b111c3bd4660292d2f93b6
SeppPenner <HansM2013@gmx.de>
2022-02-17 19:18:58 +0100

    Smaller changes.

--- src/LogCleanClient/GlobalUsings.cs
+++ src/LogCleanClient/GlobalUsings.cs
@@ -1,4 +1,4 @@
-#pragma warning disable IDE0065 // Die global using-Anweisung wurde falsch platziert.
+#pragma warning disable IDE0065 // Die using-Anweisung wurde falsch platziert.
 global using System.ComponentModel;
 global using System.Reflection;
 global using System.Xml.Linq;
@@ -6,4 +6,4 @@ global using System.Xml.Serialization;
 
 global using Languages.Implementation;
 global using Languages.Interfaces;
-#pragma warning restore IDE0065 // Die global using-Anweisung wurde falsch platziert.
+#pragma warning restore IDE0065 // Die using-Anweisung wurde falsch platziert.
